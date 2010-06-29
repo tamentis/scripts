110f0f8ef138f427b0097c707fc973cf8ce15bb7
Bertrand Janin <tamentis@neopulsar.org>
2010-06-29 11:03:43 -0400

    Added description of what the script returns.

--- fusic.py
+++ fusic.py
@@ -2,7 +2,9 @@
 """LG Fusic Status
 
 This small script makes use of the fact that the LG Fusic phone shows
-up as a serial device when plugged on a Linux box.
+up as a serial device when plugged on a Linux box. It returns a few
+details about the identity of the phone (manufacturer, model, revision and
+serial number) but also the battery level and the signal strength.
 
 You can adjust the COM_DEVICE constant below if your device is attached
 to a different device file.
