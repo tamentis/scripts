9a24708fa017854fe0284ec4c855829ddf9c7585
Bertrand Janin <tamentis@neopulsar.org>
2010-12-04 13:48:26 -0500

    Added 'usage' string in the docstring and added cron-mode.

--- litter.py
+++ litter.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
+
 """Monitor the cat litter changes.
 
 This little script reads a ~/.litterrc JSON file and returns the last time
 you changed your cat's litter.
 
+usage: litter [-c] [-h] [now]
+
+All arguments are short. The final keyword 'now' is to be added only when
+actually resetting the counter.
+
+  -c        cron mode, returns 1 and a message when the litter is overdue.
+  -h        help
 """
+
 from __future__ import with_statement
 import os.path
 import datetime
@@ -14,6 +23,7 @@ import sys
 import re
 
 
+max_days = 5
 now = datetime.datetime.now()
 date_format = "%Y-%m-%d %H:%M:%S"
 config_file = os.path.expanduser("~/.litterrc")
@@ -47,6 +57,38 @@ def get_since_datetime(dt):
     return output
 
 
+def reset_counter(last_change):
+    if last_change:
+        name = last_change["person"]
+        type = last_change["type"]
+    else:
+        name = "Anonymous"
+        type = "Brand X Litter"
+
+    new_name = raw_input("Who (default: %s): " % name)
+    new_type = raw_input("Type (default: %s): " % type)
+
+    if new_name:
+        name = new_name
+    if new_type:
+        type = new_type
+
+    changes.append({
+        "date": now.strftime(date_format),
+        "person": name,
+        "type": type
+    })
+
+    current["litter_changes"] = changes
+
+    # Do the save in two steps to avoid saving a corrupted JSON
+    dump_data = json.dumps(current, indent=4)
+    with open(config_file, "w") as fp:
+        fp.write(dump_data)
+
+    return (now + datetime.timedelta(max_days)).strftime("%A %B %d")
+
+
 if __name__ == '__main__':
     # Load the current data
     if os.path.exists(config_file):
@@ -62,47 +104,27 @@ if __name__ == '__main__':
         changes.sort(key=lambda x: x["date"])
         last_change = changes[-1]
 
-    if len(sys.argv) > 1:
-        # Change was done
-        if sys.argv[1] == "now":
-            if last_change:
-                name = last_change["person"]
-                type = last_change["type"]
-            else:
-                name = "Anonymous"
-                type = "Brand X Litter"
-
-            new_name = raw_input("Who (default: %s): " % name)
-            new_type = raw_input("Type (default: %s): " % type)
-
-            if new_name:
-                name = new_name
-            if new_type:
-                type = new_type
-
-            changes.append({
-                "date": now.strftime(date_format),
-                "person": name,
-                "type": type
-            })
-
-            current["litter_changes"] = changes
-
-            # Do the save in two steps to avoid saving a corrupted JSON
-            dump_data = json.dumps(current, indent=4)
-            with open(config_file, "w") as fp:
-                fp.write(dump_data)
-
-            next_date = (now + datetime.timedelta(5)).strftime("%A %B %d")
-            print("Next change on " + next_date)
-
-        # Help
+    # Check for cron-mode.
+    if "-c" in sys.argv:
+        date = datetime.datetime.strptime(last_change["date"], date_format)
+        if (now - date).days >= max_days:
+            print("Litter is overdue (done %sago)" % get_since_datetime(date))
+            sys.exit(-1)
         else:
-            print("usage: litter [-h] [now]")
+            sys.exit(0)
 
-    # Just show
-    else:
+    # Explicit reset
+    if sys.argv[-1] == "now":
+        next_date = reset_counter(last_change)
+        print("Next change on " + next_date)
+        sys.exit(0)
+    
+    # No parameters left, show the last reset
+    if len(sys.argv) == 1:
         date = datetime.datetime.strptime(last_change["date"], date_format)
         print("Last changed %sago" % get_since_datetime(date))
+        sys.exit(0)
 
-
+    # We've exhausted the possibilities, help!
+    print(__doc__)
+    sys.exit(-1)
